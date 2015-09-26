bfd1aea466546deaa0e3e450679cff0585a2b190
Cameron Reynoldson <Cameronreynoldson@utexas.edu>
2015-09-26 00:01:44 -0500

    Update CountingSort.java

--- CountingSort.java
+++ CountingSort.java
@@ -6,6 +6,7 @@ public class CountingSort
 	{
 		int[] a = {2,1,4,6,0,-5};
 		countingSort(a,-5,6);
+		System.out.println(Arrays.toString(a));
 	}
 
 	private static void countingSort(int[] a, int low, int high)
@@ -17,20 +18,16 @@ public class CountingSort
 		}
 
 		int[] result = new int[a.length];
-		System.out.println(Arrays.toString(count));
 		int index = 0; 
 
-		for(int i = 0; i < result.length; ++i)
+		for(int i = 0; i < count.length; ++i)
 		{
 			while(count[i] > 0 && index < result.length)
 			{
-				System.out.println(i+" "+low);
-				result[index] = i + low; 
+				a[index] = i + low; 
 				count[i]--;
 				index++;
 			}
 		}
-
-		System.out.println(Arrays.toString(result));
 	}
 }
